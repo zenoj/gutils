c64d3860284c884f1f59713a650f5b2875f0566f
darkimpostor <vera.crypt5@protonmail.com>
2022-01-07 19:39:39 +0200

    strings package added

--- baseExt/strgs/strgs.go
+++ baseExt/strgs/strgs.go
@@ -1,7 +1,20 @@
- package strgs
+package strgs
 
- func strgsToInts(l []string) []int {
-	for _,e := range l {
-		
-	} 
- }
+// CountElements counts the number of occurrences of each number
+// in the list l and adds the frequencies to the given map.
+func CountElements(frequency map[string]int, l ...string) {
+	for _, i := range l {
+		frequency[i]++
+	}
+}
+
+// DeleteElem deletes the ith element from the slice while changing the order of the slice
+func DeleteElem(a []string, i int) {
+	a[len(a)-1], a[i] = a[i], a[len(a)-1]
+	a = a[:len(a)-1]
+}
+
+// DeleteElemKeepOrder deletes the ith element from the slice while preserving the previous order
+func DeleteElemKeepOrder(a []string, i int) {
+	a = append(a[:i], a[i+1:]...)
+}
