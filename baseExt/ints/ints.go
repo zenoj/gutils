7ac029799981880d58b7917f501d4527f2cd111e
darkimpostor <vera.crypt5@protonmail.com>
2022-01-07 18:40:55 +0200

    baseExt changed

--- baseExt/ints/ints.go
+++ baseExt/ints/ints.go
@@ -1,7 +1,6 @@
 package ints
 
 import (
-	"math"
 	"strconv"
 )
 
@@ -12,54 +11,6 @@ func IntsToString(l ...int) []string {
 		a = append(a, strconv.Itoa(e))
 	}
 	return a
-}
-
-// Abs returns the absolut value of a given integers
-func Abs(e int) int {
-	if e < 0 {
-		e = -e
-	}
-	return e
-}
-
-// MaxInt returns the maximum of multiple integers
-func MaxInt(e ...int) int {
-	max := math.MinInt
-	for _, i := range e {
-		if i > max {
-			max = i
-		}
-	}
-	return max
-}
-
-// MinInt returns the minimum of multiple integers
-func MinInt(e ...int) int {
-	min := math.MinInt
-	for _, i := range e {
-		if i < min {
-			min = i
-		}
-	}
-	return min
-}
-
-// Sum computes the sum of multiple integers
-func Sum(e ...int) int {
-	res := 0
-	for _, i := range e {
-		res += i
-	}
-	return res
-}
-
-// Product computes the product of multiple integers
-func Product(e ...int) int {
-	res := 1
-	for _, i := range e {
-		res *= i
-	}
-	return res
 }
 
 // CountElements counts the number of occurrences of each number
